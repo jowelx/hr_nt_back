d82fcb0891eb199a49eaec9a55f334dd1aa1999f
jowelx <jzambranojazc2001@gmail.com>
2022-10-03 12:26:30 -0400

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const corsOptions ={
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions));
+app.use(cors());
 app.set('port', process.env.PORT || 4000)
 //middlewares
 app.use(morgan('dev'))
